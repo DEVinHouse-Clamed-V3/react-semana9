58e3718a65eb8921acee733b5af728e70b4d5fd9
˜Yan <yan.m.esteves@gmail.com>
2024-09-16 20:57:53 -0300

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [novoProduto, setNovoProduto] = useState("Caderno")
+  const [produtos, setProdutos] = useState(["Celular", "Geladeira", "Fogão"])
+  const [count, setCount] = useState(0)
+
+  function addProduto() {
+    setProdutos([...produtos, novoProduto])
+    setNovoProduto("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Produto Digitado: {novoProduto}</Text>
+      
+      <TextInput value={novoProduto} placeholder='Digite o nome do produto'
+        onChangeText={setNovoProduto} />
+
+      {produtos.map(produto => {
+        return <Text>{produto}</Text>
+      })}
+      <Button title='Adicionar Produto' onPress={addProduto} />
+
+      {/* <Text>Count: {count}!</Text> */}
       <StatusBar style="auto" />
+
+      {/* <Button title='Aumentar' 
+        onPress={() => mudarValorCount(1)} />
+
+      <Button title='Diminuir' 
+        onPress={() => mudarValorCount(-1)} /> */}
+
+
+      {/* 
+        BARRA DE PESQUISA
+
+        Lista de Produtos
+        arrayProdutos...
+
+        {array.map(item...)}
+      */}
+
     </View>
   );
 }
