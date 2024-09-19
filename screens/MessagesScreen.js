b4d9455e47a7a0e5f451c1c08247b7609ed98937
˜Yan <yan.m.esteves@gmail.com>
2024-09-19 19:40:58 -0300

    Exemplo pratico com storage

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, Button, Switch } from "react-native";
+import { getData, storeData } from "../services/storage";
 
 export default function MessagesScreen() {
+
+    const [isDark, setIsDark] = useState(false)
+
+    // Montado
+    useEffect(() => {
+        async function carregarDadosStorage() {
+            const themePreference = await getData('theme')
+            console.log('Preferencia de tema salva: ', themePreference)
+        }
+        carregarDadosStorage()
+    }, [])
+
+    function mudancaTema() {
+        console.log('Usuario está mudando a preferencia de tema. isDark: ', isDark)
+        setIsDark(!isDark)
+        storeData('theme', isDark ? 'dark' : 'light')
+    }
     return (
         <View>
             <Text>Não existem mensagens recentes.</Text>
+            <Switch value={isDark} onValueChange={mudancaTema}/>
         </View>
     )
 }
